c55679f632e253dcad0e70cee7f0cebae606becf
Vitaly Kushner <vitaly@astrails.com>
2018-08-21 00:07:58 +0200

    host local config

---
+++ custom/kenobi.zsh
@@ -0,0 +1,6 @@
+path=(
+ ~/my/dev/docker/lego/bin
+ $path
+)
+
+source ~/dev/docker/dm/dm.sh
--- custom/load.zsh
+++ custom/load.zsh
@@ -5,3 +5,7 @@ done < <(\ls ~/.zsh/custom/profile.d/*.zsh)
 while read f; do
   source $f
 done < <(\ls ~/.zsh/custom/rc.d/*.zsh)
+
+if [ -r ~/.zsh/custom/$(hostname -s).zsh ]; then
+  source ~/.zsh/custom/"$(hostname -s).zsh"
+fi
