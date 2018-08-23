a2ca349b3b58971aa12a50f9605f666dbd0a0282
Vitaly Kushner <vitaly@astrails.com>
2018-08-23 16:42:10 +0200

    update

--- custom/host/pandora.zsh
+++ custom/host/pandora.zsh
@@ -1,9 +1,2 @@
-path=(
-  ~/my/dev/docker/lego/bin
-  $path
-)
-
-source ~/dev/docker/dm/dm.sh
-
 BASE16_SHELL=$HOME/dev/src/github.com/chriskempson/base16-shell
 [ -n "$PS1" ] && [ -s "$BASE16_SHELL/profile_helper.sh" ] && eval "$("$BASE16_SHELL/profile_helper.sh")"
--- custom/load.zsh
+++ custom/load.zsh
@@ -1,3 +1,7 @@
+if [ -r ~/.zsh/custom/host/$(hostname -s).zsh ]; then
+  source ~/.zsh/custom/host/"$(hostname -s).zsh"
+fi
+
 while read f; do
   source $f
 done < <(\ls ~/.zsh/custom/profile.d/*.zsh)
@@ -5,7 +9,3 @@ done < <(\ls ~/.zsh/custom/profile.d/*.zsh)
 while read f; do
   source $f
 done < <(\ls ~/.zsh/custom/rc.d/*.zsh)
-
-if [ -r ~/.zsh/custom/host/$(hostname -s).zsh ]; then
-  source ~/.zsh/custom/host/"$(hostname -s).zsh"
-fi
