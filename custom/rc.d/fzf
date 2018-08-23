7ac443e9bb907a1b7e89c6d28dd690227841d159
Vitaly Kushner <vitaly@astrails.com>
2018-08-23 18:43:46 +0200

    fzf

--- custom/rc.d/fzf
+++
@@ -1,7 +0,0 @@
-# vim: ft=zsh
-
-export FZF_DEFAULT_COMMAND="ag --follow --nocolor --nogroup -g ''"
-if [ -e /usr/local/opt/fzf/shell/completion.zsh ]; then
-  source /usr/local/opt/fzf/shell/key-bindings.zsh
-  source /usr/local/opt/fzf/shell/completion.zsh
-fi
---
+++ custom/rc.d/fzf.zsh
@@ -0,0 +1,33 @@
+fzf_menu() {
+  fzf -0 -1 "$@"
+}
+
+fzf_cd_src() {
+  local src=~/dev/src
+
+  local list() (
+    cd $src
+    find . -type d -name .git -maxdepth 4 | xargs -n1 dirname | cut -d/ -f2-
+  )
+
+  local d=$(list | fzf_menu -q "${1:-}")
+
+  [ -n "$d"  ] || return
+
+  local d="$src/$d"
+
+  [ -d "$d"  ] || return
+
+  cd "$d"
+}
+
+fzf_base16() {
+  local theme=$(alias |  grep '^base16_[-a-zA-Z0-9_-]\+=' | cut -d= -f1 | cut -d_ -f2- | fzf_menu --cycle -q "${1:-}")
+  if [ -n "$theme" ]; then
+    echo switching to $theme
+    eval "base16_$theme"
+  fi
+}
+
+alias G=fzf_cd_src
+alias B=fzf_base16
