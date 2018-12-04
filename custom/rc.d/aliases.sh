35c0ab338710456d074dc03ec8c4ce5fd44bcd0e
Vitaly Kushner <vitaly@astrails.com>
2018-12-04 03:54:05 +0100

    aliases

---
+++ custom/rc.d/aliases.sh
@@ -0,0 +1,15 @@
+alias ddr="docker run -it --rm"
+alias dc=docker-compose
+
+
+alias cat="bat --theme TwoDark"
+
+alias aria='aria2c -s 10 -x 10 -k 1M'
+
+
+alias s3="aws s3"
+alias ec2="aws ec2"
+
+
+alias bb="afplay /System/Library/Sounds/Glass.aiff"
+alias k=kubectl
