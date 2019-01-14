534a0b2593f604cdb0e669b0df5c6b177a0fedad
Vitaly Kushner <vitaly@astrails.com>
2019-01-14 08:44:04 +0100

    skip wd config when not presetn

--- custom/rc.d/98-wd.zsh
+++ custom/rc.d/98-wd.zsh
@@ -1,16 +1,20 @@
-wd() {
-  . ~/dev/src/github.com/mfaerevaag/wd/wd.sh
-}
 unalias g
-function g()
-{
-  local dir=$(\cat ~/.warprc | cut -d: -f1 | fzf -1 -0 +i -q "$*")
 
-  if [ -n "$dir" ]; then
-    echo "Changing to $dir"
-    wd $dir
-    chruby_auto
-  else
-    echo "no match"
-  fi
-}
+if [ -e ~/dev/src/github.com/mfaerevaag/wd/wd.sh ]; then
+  wd() {
+    . ~/dev/src/github.com/mfaerevaag/wd/wd.sh
+  }
+
+  g()
+  {
+    local dir=$(\cat ~/.warprc | cut -d: -f1 | fzf -1 -0 +i -q "$*")
+
+    if [ -n "$dir" ]; then
+      echo "Changing to $dir"
+      wd $dir
+      chruby_auto
+    else
+      echo "no match"
+    fi
+  }
+fi
