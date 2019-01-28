e6043dac8aee0ccd84faec060ea1b453be326323
Vitaly Kushner <vitalyk@gmail.com>
2019-01-28 05:53:06 +0100

    ZSH_PROMPT_HOST

--- modules/prompt/init.zsh
+++ modules/prompt/init.zsh
@@ -8,6 +8,10 @@
 # Load and execute the prompt theming system.
 autoload -Uz promptinit && promptinit
 
+if p -n "${SSH_TTY:-}"; then
+  ZSH_PROMPT_HOST=1
+fi
+
 # Load the prompt theme.
 zstyle -a ':prezto:module:prompt' theme 'prompt_argv'
 if [[ "$TERM" == (dumb|linux|*bsd*) ]] || (( $#prompt_argv < 1 )); then
